8d4c27fc272439b7b8d15099424b63a878afd93a
Sumit Patel <sumitz1207@gmail.com>
2023-03-11 21:19:49 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    // eslint-disable-next-line react/react-in-jsx-scope
+    <form>
+      <input
+        id="dinosaur_name"
+        name="dinosaur_name"
+        className="dinosaur-input"
+        aria-describedby="dinosaur_name_info"
+      />
+    </form>
+  );
+}
+
+export default App;
